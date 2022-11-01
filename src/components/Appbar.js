327d8866d66e610e85ea8570e138e0aace9abe0d
Marcel Nägle <marcel.naegle@kiteit.de>
2022-11-01 09:04:41 +0100

    inital commit for FE

---
+++ src/components/Appbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+export default function Appbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Student Management System
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
