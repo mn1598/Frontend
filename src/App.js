e6529062f68fc8ea1c629ec43df77e6d86b8880e
Marcel Nägle <marcel.naegle@kiteit.de>
2022-11-06 12:37:30 +0100

    RES-5 Added new Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Appbar from './components/Appbar';
-import Student from './components/Student';
+// import Student from './components/Student';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Appbar/>
-      <Student/>
+      <Home/>
     </div>
   );
 }
